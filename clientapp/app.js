c9533d7f794d8b243c2b626845911582e89105c3
Jacob Linneman <linnemanj@xavier.edu>
2020-06-10 18:40:52 -0400

    Styles

--- clientapp/app.js
+++ clientapp/app.js
@@ -24,7 +24,7 @@ app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/login', loginRouter);
 app.use('/static', express.static('public'));
-
+app.use('/favicon.ico', express.static('images/favicon.ico'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
