fa86656bacc2751dc6018438be3104de740ced6c
BryanLikesToProgram <bryan@datandragons.io>
2020-10-10 18:31:34 -0400

    Cleanup from broken merge

--- clientapp/controllers/loginController.js
+++ clientapp/controllers/loginController.js
@@ -6,6 +6,7 @@ var user_model = require('../models/user');
 
 /// User Input validation & sanitization ///
 const validator = require('express-validator');
+const { contextsKey } = require('express-validator/src/base');
 
 const networkOptions = ["testnet", "mainnet"]
 accountID = ""
@@ -18,91 +19,64 @@ loginData = {
     'selectedNetwork': networkOptions[0]
 }
 
-exports.init = function(req, res){
-    res.render('login', { title: 'LOGIN'});
+exports.init = function (req, res) {
+    res.render('login', { title: 'LOGIN' });
 }
 
-exports.login_form_get = function(req, res, next) {     
+exports.login_form_get = function (req, res, next) {
     //res.render('login', { title: 'Create Login' });
     res.render('login', { title: 'Login', data: loginData });
-  }
+}
 
-  // Handle login form on POST.
-exports.login_form_post =  function(req, res, next) {    
-      // Validate that the fields are not empty.
-      
-      validator.check('acctId_inpt').isLength({ min: 1 });
-      validator.check('privKey_inpt', 'Private Key required').isLength({ min: 1 });
-      // Sanitize (escape) the fields.
-      validator.body('acctId_inpt').escape();
-      validator.body('privKey_inpt').escape();
-      
-      // Extract the validation errors from a request.
-      errors = validator.validationResult(req);
+failed_login = function (req, res, next) {
+    console.log("LOGIN FAILED@")
+    res.render('login', { title: 'Login', data: loginData });
+}
 
-      loginData.accountID = req.body.acctId_inpt
-      loginData.selectedNetwork = req.body.ntwrk_sel; 
-      loginData.privateKey = req.body.privKey_inpt;
+async function initAccount(req, res, next) {
 
-      if (!errors.isEmpty()) {
+    if (await HashNet.initAccount(res, errors, loginData)) {
+        user_model.setCurrentUser(loginData.accountID);
+        console.log("Getting topic info....");
+        const resp = await HashNet.getTopicInfo();
+        if (resp == false) {
+            console.log('Login failed...');
+            res.redirect('/login');
+        } else {
+            res.redirect('/user');
+        }
+
+    }
+
+}
+
+// Handle login form on POST.
+exports.login_form_post = function (req, res, next) {
+    // Validate that the fields are not empty.
+
+    validator.check('acctId_inpt').isLength({ min: 1 });
+    validator.check('privKey_inpt', 'Private Key required').isLength({ min: 1 });
+    // Sanitize (escape) the fields.
+    validator.body('acctId_inpt').escape();
+    validator.body('privKey_inpt').escape();
+
+    // Extract the validation errors from a request.
+    errors = validator.validationResult(req);
+
+    loginData.accountID = req.body.acctId_inpt
+    loginData.selectedNetwork = req.body.ntwrk_sel;
+    loginData.privateKey = req.body.privKey_inpt;
+
+    console.log("GETTING TOPIC INFO");
+    if (!errors.isEmpty()) {
         // There are errors. Render the form again with sanitized values/error messages.
-        res.render('login', { title: 'Login', data: loginData, errors: errors.array()});
+        res.render('login', { title: 'Login', data: loginData, errors: errors.array() });
         //res.render('index', { title: 'Express' });
         return;
-      }
-      else {
-        (async () => {
-            if(await HashNet.initAccount(res, errors, loginData)){
-                user_model.setCurrentUser(loginData.accountID);
-                res.redirect('/user');
-                /// Get Topic Info ///  
-                HashNet.getTopicInfo(); 
-            }else{
-                console.log("FAIL")
-            }
-        })()
-        
-      }
-        
     }
-/*
-// Display list of all Authors.
-exports.author_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author list');
-};
+    else {
+        initAccount(req, res, next).catch(error => { "hello world" });
+    }
+    console.log("GETTING TOPIC INFO");
 
-// Display detail page for a specific Author.
-exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
-};
-
-// Display Author create form on GET.
-exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
-};
-
-// Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
-
-// Display Author delete form on GET.
-exports.author_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete GET');
-};
-
-// Handle Author delete on POST.
-exports.author_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete POST');
-};
-
-// Display Author update form on GET.
-exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
-};
-
-// Handle Author update on POST.
-exports.author_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update POST');
-};
-*/
+}
--- clientapp/models/hashnet.js
+++ clientapp/models/hashnet.js
@@ -1,24 +1,25 @@
+const { contextsKey } = require('express-validator/src/base');
 var hash = require('hash-sdk');
 
-const TOPICID = "0.0.46939";
+const TOPICID = "0.0.68939";
 
 // Simple hash from string (based on Java hashCode) //
 function _hashCode(str) {
     return str.split('').reduce((prevHash, currVal) =>
-      (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
+        (((prevHash << 5) - prevHash) + currVal.charCodeAt(0)) | 0, 0);
 }
-  
 
-function _convertToHMess(rawData){
-    formStr = `|${rawData.zip}|`  
+
+function _convertToHMess(rawData) {
+    formStr = `|${rawData.zip}|`
     for (var key in rawData.bugCount) {
         let value = rawData.bugCount[key];
-        formStr = formStr + `${key}:${value};` 
+        formStr = formStr + `${key}:${value};`
     }
-    formStr = formStr.replace(/.$/,"|"); // Replace last char w/ "|"
-    hashReadyStr = Date.now().toString() + formStr ;
+    formStr = formStr.replace(/.$/, "|"); // Replace last char w/ "|"
+    hashReadyStr = Date.now().toString() + formStr;
     //console.log(_hashCode(hashReadyStr));
-    return([_hashCode(hashReadyStr), formStr]);
+    return ([_hashCode(hashReadyStr), formStr]);
 }
 
 exports.initAccount = async (res, errors, account) => {
@@ -40,47 +41,95 @@ exports.initAccount = async (res, errors, account) => {
         return true;
     } catch (e) {
         console.log('Error in intializing account:::', e);
-        res.render('login', { title: 'Login', data: loginData, errors: e.errorString});
+        res.render('login', { title: 'Login', data: loginData, errors: e.errorString });
         return false;
     }
-  }
+}
 
+//// Can't create topic id at address, actually need to get active from db
+exports.createTopic = async () => {
+    var data = new Object();
+    data.topicId = TOPICID;
+    console.log("Creating topic " + data.topicId);
+
+    return await hash.triggerTopicCreate(data, (err, res) => {
+        if (err) {
+            if (err.error.res.name == 'BadKeyError') {
+                console.log("BAD KEY ERROR")
+                return false;
+            } else {
+                //error case
+                console.log('Error:::', err);
+                return false;
+            }
+        } else {
+            //success case
+            console.log('Success:::', res);
+            //return true;
+        }
+    }).catch((error) => {
+        // Bad Password
+        if (error.error.res.name == 'BadKeyError') {
+            console.log("BAD Private Key ERROR")
+            return false;
+        } else {
+            console.log('Error:::', err);
+            return false;
+        }
+
+    });
+}
 
 exports.getTopicInfo = async () => {
     var data = new Object();
     data.topicId = TOPICID;
     console.log("Getting topic info " + data.topicId);
-    try {
-        hash.triggerTopicInfo(data, (err,res)=>{
-            if(err){
+    return await hash.triggerTopicInfo(data, (err, res) => {
+        if (err) {
+            console.log("ERROR!!!!!!! in get topic info")
+            if (err.error.res.name == 'BadKeyError') {
+                console.log("BAD KEY ERROR")
+                return false;
+            } else {
                 //error case
-                console.log('Error:::',err);
-            }else{
-                //success case
-                console.log('Success:::',res);
+                console.log('Error:::', err);
+                return false;
             }
-        });
-    } catch (e) {
-        console.log('Error in intializing account:::', e);
-        res.render('login', { title: 'Login', data: loginData, errors: e.errorString});
-        return false;
-    }
+        } else {
+            //success case
+            console.log('Success:::', res);
+            return true;
+        }
+    }).catch((error) => {
+        // Bad Password
+        if (error.error.res.name == 'BadKeyError') {
+            console.log("BAD Private Key ERROR")
+            return false;
+        } else if (error.error.res.name == 'HederaPrecheckStatusError') {
+            return (error.error.res.name)
+        }
+        else {
+            console.log('Error:::', err);
+            return false;
+        }
+
+    });
 }
 
 exports.submitMessage = async (messContent) => {
     var data = new Object();
-    data.topicId = TOPICID;   
+    data.topicId = TOPICID;
     [data.memo, data.message] = _convertToHMess(messContent);
-    if (data.message != ''){
+    if (data.message != '') {
         console.log(`Submitting Message: ${data.memo} ${data.message}`);
         data.message = `|${data.memo}${data.message}`;
-        hash.triggerMessageSubmit(data, (err,res)=>{
-            if(err){
+        hash.triggerMessageSubmit(data, (err, res) => {
+            if (err) {
                 //error case
-                console.log('Error:::',err);
-            }else{
+                console.log('Error:::', err);
+            } else {
                 //success case
-                console.log('Success:::',res);
+                console.log('Success:::', res);
             }
         });
     } else {
@@ -90,23 +139,22 @@ exports.submitMessage = async (messContent) => {
 
 exports.submitEditMessage = async (prevUID, messContent) => {
     var data = new Object();
-    data.topicId = TOPICID;    
+    data.topicId = TOPICID;
     [data.memo, data.message] = _convertToHMess(messContent);
-    if (data.message != ''){
+    if (data.message != '') {
         data.memo = prevUID
         console.log(`Submitting Message: ${data.memo} ${data.message}`);
-        data.message = `|${data.memo}${data.message}`;       
-            
-        hash.triggerMessageSubmit(data, (err,res)=>{
-            if(err){
+        data.message = `|${data.memo}${data.message}`;
+
+        hash.triggerMessageSubmit(data, (err, res) => {
+            if (err) {
                 //error case
-                console.log('Error:::',err);
-            }else{
+                console.log('Error:::', err);
+            } else {
                 //success case
-                console.log('Success:::',res);
+                console.log('Success:::', res);
             }
         });
-            
     } else {
         console.log("empty message");
     }
